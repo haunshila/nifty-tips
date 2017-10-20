1c109332f7d98a403b2818b9c6d395c9c44e7c9f
haunshi <hauyadav@cisco.com>
2017-10-20 17:20:29 +0530

    playground for swift KVO

---
+++ KVCPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    let label = UILabel()
+
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+        
+       
+    }
+}
+
+// Present the view controller in the Live View window
+let myVC = MyViewController()
+let observation = myVC.label.observe(\.text) { (lbl, change) in
+    print("new labelText: \(lbl.text)")
+}
+print(observation)
+
+PlaygroundPage.current.liveView = myVC
+myVC.label.text = "Hello"
+myVC.label.text = "Hello1"
+
+
